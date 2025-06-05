bd7d32af954e7400b471f193c2d966a23790935d
Jxon <649672019@qq.com>
2025-06-05 23:19:50 +0800

    v1

--- src/main/java/com/jxon/juscore/Juscore.java
+++ src/main/java/com/jxon/juscore/Juscore.java
@@ -1,11 +1,7 @@
 package com.jxon.juscore;
 
-import com.jxon.juscore.commands.MarkovJuniorCommand;
-import com.jxon.juscore.config.MarkovJuniorConfig;
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,48 +13,12 @@ public class Juscore implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-
-	private static MarkovJuniorConfig config;
-
 	@Override
 	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
 
-		LOGGER.info("MarkovJunior Mod initializing...");
-
-		// 加载配置文件
-		config = MarkovJuniorConfig.load();
-
-		// 注册指令
-		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-			MarkovJuniorCommand.register(dispatcher);
-		});
-
-		// 服务器启动事件
-		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-			LOGGER.info("MarkovJunior Mod loaded successfully!");
-			LOGGER.info("Use /mj help for command usage");
-		});
-
-		// 服务器关闭事件
-		ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
-			LOGGER.info("MarkovJunior Mod shutting down...");
-		});
-
-		LOGGER.info("MarkovJunior Mod initialized!");
-	}
-
-	/**
-	 * 获取配置实例
-	 */
-	public static MarkovJuniorConfig getConfig() {
-		return config;
-	}
-
-	/**
-	 * 重新加载配置
-	 */
-	public static void reloadConfig() {
-		config = MarkovJuniorConfig.load();
-		LOGGER.info("MarkovJunior configuration reloaded");
+		LOGGER.info("Hello Fabric world!");
 	}
 }
